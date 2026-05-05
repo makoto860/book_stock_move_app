fbf3b6b6e496dd0d80d187c7573b7c9baffde2d1
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2026-05-05 16:04:02 +0900

    エラーメッセージとbooks/showの画面追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :book_quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :rack_number, presence: true
+  validates :isbn, presence: true
 end
