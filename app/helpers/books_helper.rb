95c68221b6094de2b48b42a4c2b4ee6c9881f839
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2026-05-09 19:24:32 +0900

    取り寄せのとき色を変える機能を追加。

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,16 @@
 module BooksHelper
+  # app/helpers/books_helper.rb
+  def book_status_icon(book)
+    if book.invalid_order_timing?
+      content_tag(:span, "●", style: "color: #38bdf8;")
+    else
+      content_tag(:span, "×", style: "color: #94a3b8;")
+    end
+  end
+
+  def book_table_style(book)
+    if book.invalid_order_timing?
+      "background-color: #00ffff; font-weight: bold;"
+    end
+  end
 end
