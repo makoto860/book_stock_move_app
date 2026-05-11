10b55092b95b5d79218603ecf944c8c06a719763
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2026-05-11 10:39:45 +0900

    取り寄せ日時のビューを設定

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,4 @@
 module BooksHelper
-  # app/helpers/books_helper.rb
   def book_status_icon(book)
     if book.invalid_order_timing?
       content_tag(:span, "あり", style: "color: #38bdf8;" "font-weight: bold;")
