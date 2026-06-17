91b6bae9679b82a52e68e8cc5ea58a6e770cf493
Makoto Shimodate <zeon675@gmail.com>
2026-06-17 18:52:53 +0900

    モデルスペックにテストを記述

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
   factory :book do
     title { "本のタイトル" }
-
     sequence(:rack_number) { |n| "A-#{n}" }
     sequence(:isbn) { |n| "978〜#{n}" }
+
+    trait :invalid_order_timing do
+      special_order_date_time { Time.zone.parse("Y-M-D H:M") }
+      order_date_time { Time.zone.parse("Y-M-D H:M") }
+    end
+
+    trait :valid_order_timing do
+      special_order_date_time { Time.zone.parse("Y-M-D H:M") }
+      order_date_time { Time.zone.parse("Y-M-D H:M") }
+    end
   end
 end
