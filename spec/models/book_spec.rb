84570f82659db8b6b6d5f4eb2ea699441e99fe7f
Makoto Shimodate <zeon675@gmail.com>
2026-06-17 18:52:53 +0900

    モデルスペックにテストを記述

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,41 @@
-# spec/models/book_spec.rb
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  describe "validation" do
-    let!(:book) { create(:book) }
+  describe "バリデーション" do
+    context "titleが存在する場合" do
+      let(:book) { build(:book) }
 
-    it "titleが存在すると有効" do
-      expect(book).to be_valid
+      it '有効であること' do
+        expect(book).to be_valid
+      end
+    end
+
+    context "titleが存在しない場合" do
+      let(:book) { build(:book, title: nil) }
+
+      it "無効であること" do
+        expect(book).not_to be_valid
+      end
+    end
+  end
+
+  describe "invalid_order_timing?メソッド" do
+    context "取り寄せ日時が注文日時より前の場合" do
+      let(:book) do
+        create(:book, special_order_date_time: 1.day.ago, order_date_time: Time.current)
+      end
+
+      it "trueを返すこと" do
+        expect(book.invalid_order_timing?).to be true
+      end
+    end
+
+    context "取り寄せ日時が注文日時より後の場合" do
+      let(:book) { create(:book, :valid_order_timing) }
+
+      it "falseを返すこと" do
+        expect(book.invalid_order_timing?).to be false
+      end
     end
   end
 end
