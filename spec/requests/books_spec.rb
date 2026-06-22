541c753be65b3517437440d8c4833a1fb7ea4016
Makoto Shimodate <zeon675@gmail.com>
2026-06-22 16:04:34 +0900

    リクエストスペックで本を登録できるrspecを追加

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,13 +2,26 @@ require "rails_helper"
 
 RSpec.describe "Books Page", type: :request do
   describe "GET /books" do
+    let!(:book) { create(:book, title: "title") }
+
     it "正常にレスポンスを返すこと" do
       get books_path
-
-      puts "STATUS: #{response.status}"
-      puts response.body
-
       expect(response).to have_http_status(:success)
+    end
+
+    it "タイトルが含まれること" do
+      get books_path
+      expect(response.body).to include(book.title)
+    end
+  end
+
+  describe "POST /books" do
+    let(:params) do
+      { book: { title: "new_title", rack_number: "new_rack_number", isbn: "new_isbn"} }
+    end
+
+    it "本の情報を登録できること" do
+      expect { post books_path, params: params }.to change(Book, :count).by(1)
     end
   end
 end
