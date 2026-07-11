da7c560c8da7b1fa57b1fe81ee4a19c1cbeefa03
Makoto Shimodate <zeon675@gmail.com>
2026-07-11 17:49:03 +0900

    stock_moves_controller_test.rbでgetを変更

--- test/controllers/stock_moves_controller_test.rb
+++ test/controllers/stock_moves_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StockMovesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get stock_moves_index_url
+    get stock_moves_url
     assert_response :success
   end
 end
