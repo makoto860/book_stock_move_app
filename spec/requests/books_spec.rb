2c7c6ff0b6bc8869aa9dd9dcd85ec7eecb8b97c6
Makoto Shimodate <zeon675@gmail.com>
2026-08-20 18:43:14 +0900

    リクエストbook_specを修正。CSRFチェックを無効にする

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "books", type: :request do
+  describe "PATCH /books/:idの編集画面" do
+    let!(:book) { create(:book, title: "更新前タイトル") }
+
+    it "タイトルを更新できること" do
+      patch book_path(book),
+      params: { book: { title: "更新後タイトル" } }
+      expect(response).to have_http_status(:redirect)
+      expect(book.reload.title).to eq("更新後タイトル")
+    end
+  end
+
   describe "GET /booksの一覧画面" do
     let!(:book) { create(:book) }
 
