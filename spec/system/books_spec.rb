89fb435d1760d989b0ceff68d481467d446311eb
Makoto Shimodate <zeon675@gmail.com>
2026-08-23 12:29:39 +0900

    システムbooks_specにテストを追記

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -13,4 +13,94 @@ RSpec.describe "books", type: :system do
       expect(page).to have_content("×")
     end
   end
+
+  context "教科書一覧画面で取り寄せのとき" do
+    let!(:book) do
+      create(:book, special_order_date_time: Time.current, order_date_time: 1.day.from_now)
+    end
+
+    it "ありが表示されること" do
+      visit books_path
+      expect(page).to have_content("あり")
+    end
+
+    it "背景色が変更されること" do
+      visit book_path(book)
+      expect(page).to have_css("table[style='background-color: #00ffff;']")
+    end
+  end
+
+  it "教科書一覧画面にbooksデータが表示されること" do
+    visit books_path
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.rack_number)
+    expect(page).to have_content(book.isbn)
+    expect(page).to have_content(I18n.l(book.created_at, format: :datetime_jp))
+  end
+
+  it "教科書一覧画面に新しく登録するリンクが表示されること" do
+    visit books_path
+    expect(page).to have_link("教科書を新しく登録する")
+  end
+
+  it "教科書登録画面に教科書を登録するボタンが表示されること" do
+    visit new_book_path
+    expect(page).to have_button("教科書を登録する")
+  end
+
+  it "教科書登録画面に入力欄が表示されること" do
+    visit new_book_path
+    expect(page).to have_field("タイトル")
+    expect(page).to have_field("棚の番号")
+    expect(page).to have_field("ISBN")
+    expect(page).to have_field("備考欄")
+    expect(page).to have_field("取り寄せ日時")
+    expect(page).to have_field("注文日時")
+  end
+
+  it "教科書登録画面で教科書一覧に戻るをクリックすると一覧画面へ遷移すること" do
+    visit new_book_path
+    click_link "教科書一覧に戻る"
+    expect(page).to have_current_path(books_path)
+  end
+
+  it "教科書の情報画面にbookデータが表示されること" do
+    visit book_path(book)
+    expect(page).to have_content(book.id)
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.rack_number)
+    expect(page).to have_content(book.isbn)
+    expect(page).to have_content(book.note)
+  end
+
+  it "教科書の情報画面に教科書一覧リンクが表示されること" do
+    visit book_path(book)
+    expect(page).to have_link("教科書一覧")
+  end
+
+  it "教科書の編集画面で教科書の編集を完了するボタンが表示されること" do
+    visit edit_book_path(book)
+    expect(page).to have_button("教科書の編集を完了する")
+  end
+
+  it "教科書の編集画面でタイトル入力欄が表示されること" do
+    visit edit_book_path(book)
+    expect(page).to have_field("タイトル")
+    expect(page).to have_field("棚の番号")
+    expect(page).to have_field("ISBN")
+    expect(page).to have_field("備考欄")
+    expect(page).to have_field("取り寄せ日時")
+    expect(page).to have_field("注文日時")
+  end
+
+  it "教科書の読み取り画面でISBN読み取りが表示されること" do
+    visit books_scanner_path
+    expect(page).to have_content("ISBN読み取り画面")
+  end
+
+  it "教科書の読み取り画面でISBN読み取り部分のCSSが表示されること" do
+    visit books_scanner_path
+    expect(page).to have_css("#reader")
+    expect(page).to have_css("#result")
+  end
 end
