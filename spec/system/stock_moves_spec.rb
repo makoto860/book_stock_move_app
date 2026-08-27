d70c8bd40fb57128f22d60914d3d6e2a60943a8f
Makoto Shimodate <zeon675@gmail.com>
2026-08-27 11:29:24 +0900

    system/stock_moves_spec.rbで記述を修正

--- spec/system/stock_moves_spec.rb
+++ spec/system/stock_moves_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "stock_moves", type: :system do
-  describe "在庫を移動する画面" do
+  let!(:book) { create(:book) }
+  let!(:from_location) { create(:location, :warehouse) }
+  let!(:to_location) { create(:location, :pick) }
+  let!(:from_stock) do
+    create(:stock, book: book, location: from_location, quantity: 3)
+  end
+
+  before do
+    visit new_stock_move_path
+    select book.title, from: "stock_move_book_id"
+    select from_location.name, from: "stock_move_from_location_id"
+    select to_location.name, from: "stock_move_to_location_id"
+  end
+
+  describe "在庫移動確認画面" do
     let!(:book) { create(:book) }
     let!(:from_location) { create(:location, :warehouse) }
     let!(:to_location) { create(:location, :pick) }
@@ -10,12 +24,14 @@ RSpec.describe "stock_moves", type: :system do
     end
 
     before do
-      visit new_stock_move_path
+      visit stock_moves_path
       select book.title, from: "stock_move_book_id"
       select from_location.name, from: "stock_move_from_location_id"
       select to_location.name, from: "stock_move_to_location_id"
     end
+  end
 
+  describe "在庫を移動する画面" do
     it "タイトルが表示されること" do
       expect(page).to have_select("タイトル")
     end
@@ -45,20 +61,6 @@ RSpec.describe "stock_moves", type: :system do
   end
 
   describe "在庫移動確認画面" do
-    let!(:book) { create(:book) }
-    let!(:from_location) { create(:location, :warehouse) }
-    let!(:to_location) { create(:location, :pick) }
-    let!(:from_stock) do
-      create(:stock, book: book, location: from_location, quantity: 3)
-    end
-
-    before do
-      visit new_stock_move_path
-      select book.title, from: "stock_move_book_id"
-      select from_location.name, from: "stock_move_from_location_id"
-      select to_location.name, from: "stock_move_to_location_id"
-    end
-
     context "正常に在庫の移動ができるとき" do
       before do
         fill_in "stock_move_quantity", with: 1
