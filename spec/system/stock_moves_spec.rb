b57b375cd916ea14fff3649d19805400f2e189ae
Makoto Shimodate <zeon675@gmail.com>
2026-08-28 17:56:36 +0900

    system/stock_moves_spec.rbで在庫履歴の一覧画面と登録画面のテストを修正。

--- spec/system/stock_moves_spec.rb
+++ spec/system/stock_moves_spec.rb
@@ -2,15 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "stock_moves", type: :system do
   describe "在庫履歴の一覧画面" do
+    let!(:book) { create(:book) }
+    let!(:from_location) { create(:location) }
+    let!(:to_location) { create(:location) }
     let!(:stock_move) do
-      create(
-        :stock_move,
-        book: book,
-        from_location: from_location,
-        to_location: to_location,
-        quantity: 1,
-        move_type: "transfer"
-      )
+      create(:stock_move, book: book, from_location: from_location, to_location: to_location, quantity: 1, move_type: "transfer")
     end
 
     before do
@@ -22,101 +18,91 @@ RSpec.describe "stock_moves", type: :system do
     end
 
     it "移動元が表示されること" do
-      expect(page).to have_content(stock_move.from_location.name)
+      expect(page).to have_content(from_location.name)
     end
 
     it "移動先が表示されること" do
-      expect(page).to have_content(stock_move.to_location.name)
+      expect(page).to have_content(to_location.name)
     end
 
-    it "数が表示されること" do
+    it "在庫数が表示されること" do
       expect(page).to have_content(stock_move.quantity)
     end
 
     it "作成日が表示されること" do
-      expect(page).to have_content(I18n.l(stock_move.created_at, format: :datetime_jp))
-    end
-
-    it "件数が表示されること" do
-      expect(page).to have_content("件数")
-    end
-
-    it "ページネーションが表示されること" do
-      expect(page).to have_css(".pagination-link .pagination")
+      expect(page).to have_content(I18n.l(book.created_at, format: :datetime_jp))
     end
   end
 
-  let!(:book) { create(:book) }
-  let!(:from_location) { create(:location, :warehouse) }
-  let!(:to_location) { create(:location, :pick) }
-  let!(:from_stock) do
-    create(:stock, book: book, location: from_location, quantity: 3)
-  end
-
-  before do
-    visit new_stock_move_path
-    select book.title, from: "stock_move_book_id"
-    select from_location.name, from: "stock_move_from_location_id"
-    select to_location.name, from: "stock_move_to_location_id"
-  end
-
-  describe "在庫を移動する画面" do
-    it "タイトルが表示されること" do
-      expect(page).to have_select("タイトル")
+  describe "在庫移動履歴を登録する画面" do
+    before do
+      visit new_stock_move_path
     end
 
-    it "移動元が表示されること" do
-      expect(page).to have_select("移動元")
+    it "ID検索の入力欄が表示されること" do
+      expect(page).to have_field("book-id-search")
     end
 
-    it "移動先が表示されること" do
-      expect(page).to have_select("移動先")
+    it "移動元の入力欄が表示されること" do
+      expect(page).to have_select("stock_move_from_location_id")
     end
 
-    it "数が表示されること" do
-      expect(page).to have_content("数")
+    it "移動先の入力欄が表示されること" do
+      expect(page).to have_select("stock_move_to_location_id")
     end
-    
+
+    it "移動する数の入力欄が表示されること" do
+      expect(page).to have_field("stock_move_quantity")
+    end
+
     it "在庫を移動するボタンが表示されること" do
       expect(page).to have_button("在庫を移動する")
-    end
-
-    it "在庫を移動するをクリックすると在庫の移動した履歴一覧画面に遷移すること" do
-      fill_in "stock_move_quantity", with: 1
-      click_button "在庫を移動する"
-      click_button "在庫の移動を確定する"
-      expect(page).to have_current_path(stock_moves_path)
     end
   end
 
   describe "在庫移動確認画面" do
-    context "正常に在庫の移動ができるとき" do
-      before do
-        fill_in "stock_move_quantity", with: 1
-        click_button "在庫を移動する"
-        click_button "在庫の移動を確定する"
-      end
-
-      it "在庫の移動を確定すると在庫の移動履歴一覧で成功メッセージが表示されること" do
-        expect(page).to have_current_path(stock_moves_path)
-        expect(page).to have_content("教科書を移動しました")
-      end
+    let!(:book) { create(:book) }
+    let!(:from_location) { create(:location) }
+    let!(:to_location) { create(:location) }
+    let!(:stock_move) do
+      create(:stock_move, from_location_id: from_location.id, to_location_id: to_location.id, quantity: 3)
     end
 
-    context "在庫不足のとき" do
-      before do
-        fill_in "stock_move_quantity", with: 4
-        click_button "在庫を移動する"
-        click_button "在庫の移動を確定する"
-      end
+    before do
+      visit confirm_stock_moves_path(
+        book_id_search: book.id,
+        stock_move: {
+          book_id: book.id,
+          from_location_id: from_location.id,
+          to_location_id: to_location.id
+        }
+      )
+    end
 
-      it "在庫不足のメッセージが表示されること" do
-        expect(page).to have_content("在庫不足です")
-      end
+    it "教科書IDが表示されること" do
+      expect(page).to have_content(book.id)
+    end
 
-      it "確認画面へ戻ること" do
-        expect(page).to have_current_path(confirm_stock_moves_path, ignore_query: true)
-      end
+    it "タイトルが表示されること" do
+      expect(page).to have_content(book.title)
+    end
+
+    it "移動する数が表示されること" do
+      expect(page).to have_content(stock_move.quantity)
+    end
+
+    it "移動元が表示されること" do
+      expect(page).to have_content(from_location.name)
+    end
+
+    it "移動先が表示されること" do
+      expect(page).to have_content(to_location.name)
+    end
+
+    it "在庫の移動を確定するボタンがあること" do
+      expect(page).to have_button("在庫の移動を確定する")
     end
   end
 end
+
+ 
